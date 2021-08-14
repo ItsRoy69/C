66678c3510bba2545e0cf528387c750f30a2fd12
Jyotirmoy Roy <78967360+ItsRoy69@users.noreply.github.com>
2021-08-14 13:47:31 +0530

    Delete C directory

--- C/1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> 
- int main()  
-  {
-     printf("Name   : Jyotirmoy Roy\n"); 
-     printf("DOB    : october 24, 2001\n"); 
-     printf("Mobile : 91-9051538825\n"); 
-     return(0); 
-  }
